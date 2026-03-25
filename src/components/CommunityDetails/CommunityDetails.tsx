18491120f2467485319865f29307d5551fccac1d
code <code65168@gmail.com>
2026-03-26 01:22:34 +0300

    feat: add moderators to details section

--- src/components/CommunityDetails/CommunityDetails.tsx
+++ src/components/CommunityDetails/CommunityDetails.tsx
@@ -3,6 +3,8 @@ import { Cake } from '../../icons/Cake';
 import { World } from '../../icons/World';
 import { Markdown } from '../Markdown/Markdown';
 import { PageDetailsSection } from '../PageDetailsSection/PageDetailsSection';
+import { Fragment } from 'react/jsx-runtime';
+import { Link } from 'wouter';
 
 export function CommunityDetails({ community }: { community: GetCommunityResponse | GetPostResponse }) {
   return (
@@ -44,6 +46,40 @@ export function CommunityDetails({ community }: { community: GetCommunityRespons
 
         <div className="text-sm text-neutral-content-weak">
           <Markdown>{community.community_view.community.description}</Markdown>
+        </div>
+
+        <hr className="border-neutral-border-weak my-4"></hr>
+
+        <div className="flex flex-col gap-2 pt-4">
+          <h3 className="mb-3 text-xs font-semibold text-neutral-content-weak">MODERATORS</h3>
+          {community.moderators.map((moderator) => {
+            const creatorAbsoluteName = moderator.moderator.local
+              ? moderator.moderator.name
+              : `${moderator.moderator.name}@${new URL(moderator.moderator.actor_id).host}`;
+            return (
+              <Fragment key={moderator.moderator.id}>
+                <div className="flex items-center gap-2">
+                  {moderator.moderator.avatar && (
+                    <img className="size-8 rounded-full" src={moderator.moderator.avatar} alt="" />
+                  )}
+
+                  {!moderator.moderator.avatar && (
+                    <div className="flex justify-center items-center size-8 rounded-full bg-brand-background">
+                      <span className="text-neutral-content-strong">{moderator.moderator.name[0]?.toUpperCase()}</span>
+                    </div>
+                  )}
+
+                  <div className="flex flex-col justify-center">
+                    <Link className="text-sm text-neutral-content hover:underline" href={`/u/${creatorAbsoluteName}`}>
+                      u/{creatorAbsoluteName}
+                    </Link>
+
+                    <span className="text-xs text-secondary-weak">{moderator.moderator.display_name}</span>
+                  </div>
+                </div>
+              </Fragment>
+            );
+          })}
         </div>
       </div>
     </PageDetailsSection>
