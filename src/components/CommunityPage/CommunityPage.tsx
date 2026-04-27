a6cac3a1a730ac6f5e7803ba1574bd53543bb5d0
code <code65168@gmail.com>
2026-04-27 10:51:39 +0300

    feat: add follow community feature

--- src/components/CommunityPage/CommunityPage.tsx
+++ src/components/CommunityPage/CommunityPage.tsx
@@ -1,4 +1,10 @@
-import { useInfiniteQuery, usePrefetchInfiniteQuery, useQuery } from '@tanstack/react-query';
+import {
+  useInfiniteQuery,
+  useMutation,
+  usePrefetchInfiniteQuery,
+  useQuery,
+  useQueryClient,
+} from '@tanstack/react-query';
 import { Plus } from 'lucide-react';
 import { communityQueries, postQueries } from '../../queries';
 import { PostsSection } from '../PostsSection/PostsSection';
@@ -7,11 +13,12 @@ import { PageInfoPanel } from '../PageInfoPanel/PageInfoPanel';
 import { useIntersectionObserver } from 'usehooks-ts';
 import { PostFilterSection } from '../PostFilterSection/PostFilterSection';
 import { usePostFilterParams } from '../../usePostFilterParams';
-import type { PostSortType } from 'lemmy-js-client';
+import type { FollowCommunity, PostSortType } from 'lemmy-js-client';
 import { Sidebar } from '../Sidebar/Sidebar';
 import { useEffect } from 'react';
 import { useRecentCommunities } from '../../hooks/useRecentCommunities';
 import clsx from 'clsx';
+import { client } from '../../client';
 
 const MAX_RECENT_COMMUNITIES = 10;
 
@@ -27,6 +34,7 @@ export function CommunityPage({ name }: { name: string }) {
 function CommunityMain({ name }: { name: string }) {
   const { sort } = usePostFilterParams();
   const [, setRecentCommunities] = useRecentCommunities();
+  const queryClient = useQueryClient();
 
   const {
     data: community,
@@ -45,6 +53,12 @@ function CommunityMain({ name }: { name: string }) {
       community_name: name,
     }),
   );
+
+  const joinMutation = useMutation({
+    mutationFn: async (variables: FollowCommunity) => {
+      return client.followCommunity(variables);
+    },
+  });
 
   useEffect(() => {
     if (!community) {
@@ -82,13 +96,33 @@ function CommunityMain({ name }: { name: string }) {
     return <p>Error, something went wrong</p>;
   }
 
+  const communityData = community.community_view;
+
+  const joinCommunity = () => {
+    joinMutation.mutate(
+      {
+        community_id: community.community_view.community.id,
+        follow: community.community_view.subscribed === 'Subscribed' ? false : true,
+      },
+      {
+        onSettled: () => {
+          void queryClient.invalidateQueries(
+            communityQueries.detail({
+              name,
+            }),
+          );
+        },
+      },
+    );
+  };
+
   return (
     <div className="px-12 mt-2 max-w-290 mx-auto">
       <div className="w-full h-32">
-        {community.community_view.community.banner ? (
+        {communityData.community.banner ? (
           <img
             className="h-full w-full object-cover object-center rounded-md"
-            src={community.community_view.community.banner}
+            src={communityData.community.banner}
             alt=""
           />
         ) : (
@@ -97,24 +131,22 @@ function CommunityMain({ name }: { name: string }) {
 
         <div className="flex justify-between relative py-3">
           <div className="flex justify-center items-center gap-4 pl-28">
-            {community.community_view.community.icon && (
+            {communityData.community.icon && (
               <img
                 className="size-24 border-4 absolute left-4 bottom-2 bg-neutral-background border-neutral-background rounded-full"
-                src={community.community_view.community.icon}
+                src={communityData.community.icon}
                 alt=""
               />
             )}
 
-            {!community.community_view.community.icon && (
+            {!communityData.community.icon && (
               <div className="flex justify-center items-center gap-0.5 size-18 outline-8 absolute left-4 bottom-2 text-5xl leading-12 font-extrabold bg-neutral-content text-neutral-background border-neutral-background rounded-full">
                 <span className="mb-3">c</span>
                 <span className="mb-5">/</span>
               </div>
             )}
 
-            <h2 className="text-3xl font-bold text-neutral-content-strong">
-              c/{community.community_view.community.name}
-            </h2>
+            <h2 className="text-3xl font-bold text-neutral-content-strong">c/{communityData.community.name}</h2>
           </div>
 
           <div className="flex justify-center items-center gap-3">
@@ -127,15 +159,32 @@ function CommunityMain({ name }: { name: string }) {
             </button>
 
             <button
-              className="py-2 px-3.5 text-sm text-global-white font-bold bg-primary-background rounded-3xl hover:bg-primary-background-hover"
+              onClick={() => {
+                joinCommunity();
+              }}
+              className={clsx(
+                'py-2 px-3.5 text-sm text-global-white font-bold rounded-3xl border border-neutral-border-medium bg-neutral-background hover:bg-neutral-background-hover hover:border-secondary-plain-hover active:bg-neutral-background-selected',
+                communityData.subscribed !== 'Subscribed' && 'bg-primary-background hover:bg-primary-background-hover',
+              )}
               type="button"
             >
-              Join
+              {(() => {
+                switch (communityData.subscribed) {
+                  case 'Subscribed':
+                    return 'Joined';
+                  case 'ApprovalRequired':
+                    return 'Waiting approval';
+                  case 'Pending':
+                    return 'Pending';
+                  case 'NotSubscribed':
+                    return 'Join';
+                }
+              })()}
             </button>
           </div>
         </div>
       </div>
-      <div className={clsx('grid grid-cols-[3fr_1fr]', community.community_view.community.banner && 'mt-16')}>
+      <div className={clsx('grid grid-cols-[3fr_1fr]', communityData.community.banner && 'mt-16')}>
         <div className="flex flex-col max-w-167.5">
           <PostFilterSection />
           <CommunityPosts communityName={name} sort={sort} />
